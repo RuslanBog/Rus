1e4731e92be2a84f6f0a358961a9a6ecfe260173
Kate Brytan <brytankateryna@gmail.com>
2021-09-18 17:19:38 +0300

    my lesson_1.py

--- lesson_1.py
+++ lesson_1.py
@@ -1,3 +1,57 @@
-print("Hello, World!")
-# comment
-print("Hello, World!")
+bool_value = False
+
+print(bool_value, type(bool_value))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# value_1 = "25"
+# value_2 = "2"
+
+# value = value_1 / value_2 # обычное деление
+# value = value_1 // value_2 # целочисленное деление (без округления)
+# value = value_1 % value_2 # остаток от деления
+# value = value_1 ** value_2 # возведение в степень. Корень для ** 0.5
+# value = value_1 + value_2
+# print(value) #, type(value))
+
+# value = "100"
+# value = 100
+# print(value, type(value))
+
+# first = "qwe"
+# second = 1
+# value = first
+# first = second
+# second = value
+
+# first, second = second, first
+
+
+# print("Hello, World!")
+# # comment
+# print("Hello, World!")
+# print('Hello!')
+#
+#
+# print(123 * 2, 456, 789, "qwe")
+# print(12 - 3)
+# print(23 + 5 * 10)
