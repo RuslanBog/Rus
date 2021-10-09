a84e76244fc72929cdbee897a61de4a89da4b996
Kate Brytan <brytankateryna@gmail.com>
2021-10-09 17:33:56 +0300

    lesson 4_2

--- Lesson_4.py
+++ Lesson_4.py
@@ -93,3 +93,65 @@
 # print(chr(34))
 
 # генератор списков (list comprehension)
+
+# alphabet_list = []
+# for index_ascii in range(ord('a'), ord('z') + 1):
+#     alphabet_list.append(chr(index_ascii))
+####################
+# alphabet_list = [chr(index_ascii) for index_ascii in range(ord('a'), ord('z') + 1)]
+#
+# alphabet = "".join(alphabet_list)
+# print(alphabet)
+################################
+# result = [x ** 2 for x in range(10)]
+# print(result)
+
+# my_list = [12, -45, 23, 5, 0, 21, 900]
+
+# res = []
+# for value in my_list:
+#     if value < 10:
+#         res.append(value)
+# res = [value for value in my_list if value < 10]
+#
+#
+# print(res)
+ ################# множества (set) - изменяемый тип, толбко один представитель для каждого объекта, порядок не сохраняется
+
+# my_list = [1, 2, 3, 4, 5, 5, 5, "1"]
+#
+# my_set = set(my_list)
+# print(my_set, type(my_set))
+####################################
+# 3) через set
+# my_str = "bla BLA car"
+# lower_str = my_str.lower()
+#
+#
+# unique_symbols = set(lower_str) ### порядок не сохраняется
+# print(unique_symbols)
+#
+# unique_symbols_count = len(unique_symbols)
+# print(unique_symbols_count)
+# #
+# print(unique_symbols_count) не закончено#########
+
+# my_list = [1, 2, 3, 4, 5, 5, 5, "1"]
+#
+# my_set = list(set(my_list)) #убрать дубли в списке
+# print(my_set, type(my_set))
+####################################
+
+my_str_1 = 'jhgghkgkglkdjdj'
+my_str_2 = 'kflsb,xbmnvbjbkjdfbvnb  dfv'
+my_str_1_set = set(my_str_1)
+my_str_2_set = set(my_str_2)
+
+same_symbol = my_str_1_set.intersection(my_str_2_set)
+print(same_symbol)
+
+all_symbols = my_str_1_set.union(my_str_2_set)
+print(all_symbols)
+
+first_str_unique = my_str_1_set.difference(my_str_2_set)
+print(first_str_unique)
